581c857d70f115d7ccc24ede6091fc36395a49aa
Jeremiah Crouse <jeremiah.crouse@outlook.com>
2026-04-07 06:53:50 -0500

    Updated music again

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -24,7 +24,7 @@ export default function Audio() {
     <div className="audio-player">
       <audio 
         ref={audioRef} 
-        src="/instrumental.m4a" 
+        src="/instrumental2.m4a" 
         loop // This attribute ensures the music restarts automatically
       />
       <button className="audio-toggle" onClick={togglePlay}>
