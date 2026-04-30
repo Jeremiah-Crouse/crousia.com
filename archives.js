7cec9635bd30cd4f25192b9f07c51263967a01ab
Jeremiah Crouse <jeremiah.crouse@outlook.com>
2026-04-29 19:42:23 -0500

    😊

--- archives.js
+++ archives.js
@@ -2,9 +2,12 @@ import express from 'express';
 import fs from 'fs';
 import fsp from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const router = express.Router();
-const ARCHIVES_DIR = '/Users/jeremiahcrouse/crousia-v2/archives';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const ARCHIVES_DIR = path.join(__dirname, 'archives');
 
 /**
  * GET /archives
