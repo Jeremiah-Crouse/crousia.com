f36f4f2f0ea87be8e5a69989e7889a10afca1d3c
Jeremiah Crouse <jeremiah.crouse@outlook.com>
2026-05-10 11:56:59 -0500

    Change music

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -24,7 +24,7 @@ export default function Audio() {
     <div className="audio-player">
       <audio 
         ref={audioRef} 
-        src="/1223.m4a" 
+        src="/quickbrownfox.m4a" 
         loop // This attribute ensures the music restarts automatically
       />
       <button className="audio-toggle" onClick={togglePlay}>
